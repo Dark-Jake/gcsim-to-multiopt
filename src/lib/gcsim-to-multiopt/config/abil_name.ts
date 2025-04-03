e547a8f9ed30bd10262fd614bd476d8eb23d3126
DarkJake <thedark.tutos11@gmail.com>
2025-04-03 11:50:34 -0600

    Add new abilities to the conversion map

--- src/lib/gcsim-to-multiopt/config/abil_name.ts
+++ src/lib/gcsim-to-multiopt/config/abil_name.ts
@@ -10,26 +10,63 @@ const defaultAbils: AbilsType = {
 
     //TODO reactions
     "bloom": ["reaction", "bloom"],
+    "hyperbloom": ["reaction", "hyperbloom"],
     "overload": ["reaction", "overloaded"],
 
     //TODO passives/skills
-    //!-------------------Arlecchino-------------------//
+    //?---------------------Albedo---------------------//
+    "Abiogenesis: Solar Isotoma": ["skill", "dmg"],
+    "Abiogenesis: Solar Isotoma (Tick)": ["skill", "blossom"],
+
+    //?---------------------Alhaitam-------------------//
+    "Charge 1": ["charged", "dmg"],
+    "Universality: An Elaboration on Form": ["skill", "rushDmg"],
+    "Chisel-Light Mirror: Projection Attack 1": ["skill", "mirrorDmg1"],
+    "Chisel-Light Mirror: Projection Attack 2": ["skill", "mirrorDmg1"],
+    "Chisel-Light Mirror: Projection Attack 3": ["skill", "mirrorDmg1"],
+    "Particular Field: Fetters of Phenomena": ["burst", "instanceDmg"],
+    
+    //?---------------------Aloy-----------------------//
+    //?---------------------Amber----------------------//
+
+    //?-------------------Arlecchino-------------------//
     "Blood Debt Directive": ["skill", "sigilDmg"],
     "All is Ash (Spike)": ["skill", "spikeDmg"],
     "All is Ash (Cleave)": ["skill", "finalDmg"],
-
-    //!---------------------Bennett--------------------//
+    
+    //?---------------------Ayato----------------------//
+    //?---------------------Baizhu---------------------//
+    //?---------------------Barbara--------------------//
+    //?---------------------Beidou---------------------//
+    
+    //?---------------------Bennett--------------------//
     "Passion Overload (Press)": ["skill", "press"],
     "Fantastic Voyage": ["burst", "dmg"],
-
-    //!--------------------Chevreuse-------------------//
+    
+    //?---------------------Candace--------------------//
+    //?---------------------Charlotte------------------//
+    //?---------------------Chasca---------------------//
+    
+    //?--------------------Chevreuse-------------------//
     "Sniper Induced Explosion (C2)": ["constellation2", "dmg"],
     "Surging Blade (Chevreuse)": ["skill", "bladeDmg"],
     "Short-Range Rapid Interdiction Fire [Overcharged]": ["skill", "holdDmg"],
     "Secondary Explosive Shell": ["burst", "shellDmg"],
     "Explosive Grenade": ["burst", "grenadeDmg"],
+    
+    //?---------------------Chongyun-------------------//
+    //?---------------------Clorinde-------------------//
+    //?---------------------Collei---------------------//
+    //?---------------------Cyno-----------------------//
+    //?---------------------Dehya----------------------//
+    //?---------------------Diluc----------------------//
+    //?---------------------Diona----------------------//
+    //?---------------------Dori-----------------------//
+    //?---------------------Emilie---------------------//
+    //?---------------------Eula-----------------------//
+    //?---------------------Faruzan--------------------//
 
-    //!---------------------Fischl---------------------//
+    //?---------------------Fischl---------------------//
     "Fischl A4": ["passive2", "dmg"],
     "Fischl C1": ["constellation1", "dmg"],
     "Her Pilgrimage of Bleak (C4)": ["constellation4", "burstAdditionalDmg"],
@@ -37,26 +74,130 @@ const defaultAbils: AbilsType = {
     "Midnight Phantasmagoria": ["burst", "dmg"],
     "Oz (Burst)": ["skill", "ozDmg"],
 
-    //!---------------------Furina---------------------//
+    //?---------------------Freminet-------------------//
+
+    //?---------------------Furina---------------------//
     "Spiritbreath Thorn (Furina)": ["normal", "thornBladeDmg"],
+    "Charge Pneuma": ["charged", "dmg"], //! I dont sure this is correct
+    "Charge Ousia": ["charged", "dmg"], //! I dont sure this is correct
     "Salon Solitaire: Ousia Bubble": ["skill", "bubbleDmg"],
     "Salon Member: Surintendante Chevalmarin": ["skill", "chevalDmg"],
     "Salon Member: Gentilhomme Usher": ["skill", "usherDmg"],
     "Salon Member: Mademoiselle Crabaletta": ["skill", "crabDmg"],
     "Let the People Rejoice": ["burst", "skillDmg"],
 
-    //!---------------------Mavuika--------------------//
+    //?---------------------Gaming---------------------//
+    //?---------------------Ganyu----------------------//
+
+    //?---------------------Gorou----------------------//
+    "Inuzaka All-Round Defense": ["skill", "dmg"],
+    "Juuga: Forward Unto Victory": ["burst", "dmg"],
+    "Crystal Collapse": ["burst", "crystalCollapse"],
+    
+    //?---------------------Heizou---------------------//
+
+    //?---------------------Hu Tao---------------------//
+    "Blood Blossom": ["skill", "dmg"],
+
+    //?---------------------Itto-----------------------//
+    "Arataki Kesagiri Final Slash": ["charged", "akFinal"],
+    "Arataki Kesagiri Combo Slash": ["charged", "akSlash"],
+    "Masatsu Zetsugi: Akaushi Burst!": ["skill", "dmg"],
+    
+    //?---------------------Jean-----------------------//
+    //?---------------------Kaeya----------------------//
+    //?---------------------Kaveh----------------------//
+    //?---------------------Kazuha---------------------//
+    //?---------------------Keqing---------------------//
+    //?---------------------Kinich---------------------//
+    //?---------------------Kirara---------------------//
+    //?---------------------Klee-----------------------//
+    //?---------------------Kuki-----------------------//
+    //?---------------------Layla----------------------//
+
+    //?---------------------Lisa-----------------------//
+    "Lightning Rose (Initial)": ["burst", "summon"],
+    "Lightning Rose (Tick)": ["burst", "tick"],
+
+    //?---------------------Lynette--------------------//
+    //?---------------------Lyney----------------------//
+    
+    //?---------------------Mavuika--------------------//
     "Flamestrider Charged Attack (Cyclic)": ["skill", "chargedCyclicDmg"],
     "Flamestrider Charged Attack (Final)": ["skill", "chargedFinalDmg"],
+    "Flamestrider Sprint": ["skill", "sprintDmg"],
     "Sunfell Slice": ["skill", "skillDmg"],
     "The Named Moment": ["skill", "skillDmg"],
     "Rings of Searing Radiance": ["skill", "radianceDmg"],
+    "Flamestrider (C6)": ["constellation6", "flamestriderDmg"],
+    "Rings of Searing Radiance (C6)": ["constellation6", "ringDmg"],
 
-    //!---------------------Xilonen--------------------//
+    //?---------------------Mika-----------------------//
+    //?---------------------Mona-----------------------//
+
+    //?---------------------Mualani--------------------//
+    //!"Surfing Hit": ["type", "subtype"],
+    "Sharky's Surging Bite": ["skill", "surgingDmg"],
+    "Boomsharka-laka": ["burst", "dmg"],
+
+    //?---------------------Nahida---------------------//
+    "All Schemes to Know (Press)": ["skill", "pressDmg"],
+    "Tri-Karma Purification": ["skill", "karmaDmg"],
+    "Tri-Karma Purification: Karmic Oblivion": ["constellation6", "dmg"],
+
+    //?---------------------Navia----------------------//
+    //!---------------------Neuvillette----------------//
+    //!---------------------Nilou----------------------//
+    //?---------------------Ningguang------------------//
+    //?---------------------Noelle---------------------//
+    //?---------------------Ororon---------------------//
+    //?---------------------Qiqi-----------------------//
+    //?---------------------Raiden---------------------//
+    //?---------------------Razor----------------------//
+    //?---------------------Sara-----------------------//
+    //?---------------------Sayu-----------------------//
+    //?---------------------Sethos---------------------//
+    //?---------------------Shenhe---------------------//
+    //?---------------------Sigewinne------------------//
+    //?---------------------Sucrose--------------------//
+    //?---------------------Tartaglia------------------//
+    //?---------------------Thoma----------------------//
+    //?---------------------Tighnari-------------------//
+    //?---------------------Traveler (Anemo)-----------//
+    //?---------------------Traveler (Cryo)------------//
+    //?---------------------Traveler (Dendro)----------//
+    //?---------------------Traveler (Electro)---------//
+    //?---------------------Traveler (Geo)-------------//
+    //?---------------------Traveler (Hydro)-----------//
+    //?---------------------Traveler (Pyro)------------//
+    //?---------------------Venti----------------------//
+    //?---------------------Wanderer-------------------//
+    //?---------------------Wriothesley----------------//
+    //?---------------------Xiangling------------------//
+    //?---------------------Xiangyun-------------------//
+    //?---------------------Xiao-----------------------//
+    
+    //?---------------------Xilonen--------------------//
     "Yohual's Scratch": ["skill", "rushDmg"],
     "Blade Roller 0": ["normal", "ns0"],
     "Blade Roller 1": ["normal", "ns1"],
     "Ocelotlicue Point!": ["burst", "skillDmg"],
+    
+    //?---------------------Xingqiu--------------------//
+    //?---------------------Xinyan---------------------//
+    //?---------------------Yae Miko-------------------//
+    //?---------------------Yanfei---------------------//
+    //?---------------------Yaoyao---------------------//
+
+    //?---------------------Yelan----------------------//
+    "Lingering Lifeline": ["skill", "dmg"],
+    "Depth-Clarion Dice": ["burst", "pressDmg"],
+    "Exquisite Throw": ["burst", "throwDmg"],
+
+    //?---------------------Yoimiya--------------------//
+    //?---------------------Yun Jin--------------------//
+    //?---------------------Zhongli--------------------//
+    
 
     //TODO weapons
     "Viridescent": ["weapon:TheViridescentHunt", "dmg"],
